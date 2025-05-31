ad227a33d5e26c3c3a6b2de5159a324bbd574280
CoolFixCoder <113441129+CoolFixCoder@users.noreply.github.com>
2025-05-31 13:41:58 +0530

    Create typedefs.h
    Typedefs of data type

---
+++ typedefs.h
@@ -0,0 +1,16 @@
+// typedefs of the values being used in the Model to represent what it actually in finance world represents
+#ifndef QL_INTEGER
+#    define QL_INTEGER int
+#endif
+ 
+#ifndef QL_BIG_INTEGER
+#    define QL_BIG_INTEGER long
+#endif
+ 
+#ifndef QL_REAL
+#   define QL_REAL double
+#endif
+ 
+typedef QL_REAL Real  // real number
+typdef Real Decimal; // decimal number
+typedef std::size)t Size // size of container
